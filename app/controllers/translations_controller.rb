4c221e1aecf655b06cd9e97277a6536750a9f952
Frederico Araujo <fred.the.master@gmail.com>
2010-06-12 13:09:13 +0700

    added more fields to CSV import

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -52,13 +52,32 @@ class TranslationsController < ApplicationController
     require "fastercsv"
     file = params[:csv_file][:file]
     arr_of_arrs = FasterCSV.read(file.path)
-    @source_language = Language.find(:first, :conditions => ["title = ?", arr_of_arrs[0][0].to_s])
-    @target_language = Language.find(:first, :conditions => ["title = ?", arr_of_arrs[0][1].to_s])
+    # de_de, en_gb,category,company, notes
+    @source_language = Language.find_or_create_by_title(arr_of_arrs[0][0].to_s)
+    @target_language = Language.find_or_create_by_title(arr_of_arrs[0][1].to_s)
     arr_of_arrs.delete_at(0)
     @total = 0
     arr_of_arrs.each do |row|
-      if (Translation.create( :source_content => row[0], :target_content => row[1], 
-        :source_language_id => @source_language.id, :target_language_id => @target_language.id))
+      @source_content = row[0].to_s
+      @target_content = row[1].to_s
+      
+      @source_content = @source_content.toutf8 unless @source_content.is_utf8?
+      @target_content = @target_content.toutf8 unless @target_content.is_utf8?
+      
+      @category_id = nil
+      @company_id = nil
+      @category_id = Category.find_or_create_by_title(row[2].to_s).id unless row[2].to_s.blank?
+      @company_id = Company.find_or_create_by_title(row[3].to_s) unless row[3].to_s.blank?
+      @notes = row[4].to_s
+      if (Translation.create( 
+        :source_content => @source_content, 
+        :target_content => @target_content, 
+        :source_language_id => @source_language.id, 
+        :target_language_id => @target_language.id,
+        :category_id => @category_id,
+        :company_id => @company_id,
+        :notes => @notes)
+        )
         @total+=1
       end
     end
