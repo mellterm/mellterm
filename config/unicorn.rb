45d5e8919fceb526f54351b6a5ab216645602c77
Frederico Araujo <fred.the.master@gmail.com>
2010-11-14 00:35:14 +0700

    increased unicorn threads to 3

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@
 
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
-worker_processes 1
+worker_processes 3
 
 # Help ensure your application will always spawn in the symlinked
 # "current" directory that Capistrano sets up.
