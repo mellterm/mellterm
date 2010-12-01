57850484be32e947c326b4f0275364dde25441d3
Frederico Araujo <fred.the.master@gmail.com>
2010-12-01 18:06:22 +0700

    no need to add names on code, use 'git blame' instead

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
-# Giang Nguyen
 class Project < ActiveRecord::Base
   has_many                :tasks
   belongs_to              :company
-  has_many_and_belongs_to :users
+  # has_many_and_belongs_to :users
+  has_and_belongs_to_many :users
   belongs_to              :pt_status
 end
 
