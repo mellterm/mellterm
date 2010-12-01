0ca2a237cabcdb6d961264cf02af66b22715c69c
Frederico Araujo <fred.the.master@gmail.com>
2010-12-01 18:42:14 +0700

    owner_id to Projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,8 @@ class Project < ActiveRecord::Base
   # has_many_and_belongs_to :users
   has_and_belongs_to_many :users
   belongs_to              :pt_status
+  
+  # This is the user that create the Project, or Project Owner
+  belongs_to  :owner,   :foreign_key => "owner_id",   :class_name => "User"
+  
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101129190948) do
+ActiveRecord::Schema.define(:version => 20101201112900) do
 
   create_table "attachments", :force => true do |t|
     t.string   "title"
@@ -135,6 +135,7 @@ ActiveRecord::Schema.define(:version => 20101129190948) do
     t.float    "rate"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "owner_id"
   end
 
   create_table "projects_users", :force => true do |t|
@@ -243,13 +244,13 @@ ActiveRecord::Schema.define(:version => 20101129190948) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "login",               :default => "",    :null => false
-    t.string   "email",               :default => "",    :null => false
-    t.string   "crypted_password",    :default => "",    :null => false
-    t.string   "password_salt",       :default => "",    :null => false
-    t.string   "persistence_token",   :default => "",    :null => false
-    t.string   "single_access_token", :default => "",    :null => false
-    t.string   "perishable_token",    :default => "",    :null => false
+    t.string   "login",                                  :null => false
+    t.string   "email",                                  :null => false
+    t.string   "crypted_password",                       :null => false
+    t.string   "password_salt",                          :null => false
+    t.string   "persistence_token",                      :null => false
+    t.string   "single_access_token",                    :null => false
+    t.string   "perishable_token",                       :null => false
     t.integer  "login_count",         :default => 0,     :null => false
     t.integer  "failed_login_count",  :default => 0,     :null => false
     t.datetime "last_request_at"
